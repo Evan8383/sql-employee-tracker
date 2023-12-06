d82f368ac4a485773a6fbe5696e2be62d33d6c70
Evan <evanbennett83@gamil.com>
2023-12-06 14:50:26 -0500

    Added all seed data.

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE department (
+CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary FLOAT NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE role (
     FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,30 @@
 INSERT INTO
-    department (name)
-VALUE
-    ()
+    department (name) VALUE ('Customer Service'),
+    ('Accounting'),
+    ('Sales'),
+    ('Development');
 
 INSERT INTO
-    role (title, salary, department_id)
-VALUE
-    ()
+    role (title, salary, department_id) VALUE 
+    ('Level 1 Tech', 50000, 1),
+    ('Level 2 Tech', 65000, 1),
+    ('Accountant', 80000, 2),
+    ('Account Manager', 95000, 2),
+    ('Sales Consultant', 75000, 3),
+    ('Sales Director', 150000, 3),
+    ('FrontEnd Engineer', 85000, 4),
+    ('BackEnd Engineer', 95000, 4),
+    ('Project Manager', 150000, 4);
 
 INSERT INTO
-    employee (first_name, last_name, role_id, manager_id)
-VALUE
-    ()
+    employee (first_name, last_name, role_id, manager_id) VALUE 
+    ('Archie', 'Atkins', 4, NULL),
+    ('Edie', 'Harmon', 6, NULL),
+    ('Harmony', 'Snow', 9, NULL),
+    ('Sean', 'Jenkins', 1, 3),
+    ('Isaac', 'Padilla', 2, 3),
+    ('Peggy', 'Potts', 3, 1),
+    ('Bruce', 'Combs', 5, 2),
+    ('Jakob', 'Faulkner', 7, 3),
+    ('Roseanna', 'Dickson', 8, 3),
+    ('Larissa', 'Fleming', 8, 3);
