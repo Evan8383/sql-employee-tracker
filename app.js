81a4754751d6f2fdc48b890b40c332c3d90ffd12
Evan <evanbennett83@gmail.com>
2023-12-06 21:04:53 -0500

    Created prompting function. Created switch statement to handle selected function. Created viewAllDepartments function.

---
+++ app.js
@@ -0,0 +1,75 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const ui = new inquirer.ui.BottomBar();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'root',
+        database: 'employee_tracker'
+    },
+    console.log(`Connected to the courses_db database.`)
+).promise();
+
+const dbFunctions = [
+    {
+        name: 'selectedFunction',
+        message: 'Choose a database function:',
+        type: 'list',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+    }
+];
+
+const getSelectedFunction = async () => {
+    const data = await inquirer.prompt(dbFunctions)
+    const { selectedFunction } = data
+    handleSelectedFunction(selectedFunction)
+}
+
+const handleSelectedFunction = (selectedFunction) => {
+    switch (selectedFunction) {
+        case 'View all departments':
+            viewAllDepartments();
+            break;
+        case 'View all roles':
+            getSelectedFunction();
+            break;
+        case 'View all employees':
+            getSelectedFunction();
+            break;
+        case 'Add a department':
+            getSelectedFunction();
+            break;
+        case 'Add a role':
+            getSelectedFunction();
+            break;
+        case 'Add an employee':
+            getSelectedFunction();
+            break;
+        case 'Update an employee role':
+            getSelectedFunction();
+            break;
+        case 'Exit':
+            process.exit();
+    }
+}
+
+const viewAllDepartments = async () => {
+    const [results, info] = await db.query('SELECT * FROM department')
+    console.table(results)
+    getSelectedFunction();
+}
+
+getSelectedFunction()
